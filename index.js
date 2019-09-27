aa3d3767709b06441e39703294103b780eed44b2
anna-kuzii <anna.kuzii@ucb.com>
2019-09-27 14:11:44 +0300

    with token

--- index.js
+++ index.js
@@ -27,6 +27,45 @@ app.get('/webhook/', function(req, res) {
     res.send('wrong token')
 })
 
+app.post('/webhook/', function(req,res) {
+    let messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i];
+        let sender = event.sender.id;
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            sendText(sender, "Text echo: " + text.substring(0, 100));
+        }
+    }
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+})
+
+const token = "EAAG2C4EMa4QBAEoIhRBxZC4HBQaYGy90ZCDyZBMs3UPECVhL5bx4whJZBk14SmQzyroWv02U1ZC4NMGMxwyFtCSQZAkzBDZAbkSAuTWze3L2Lwq4a5lY84fME09TTciUwfZC3YZBKslR2yTbEAm0UGGqGIoIs8I6Sq5WS9fq1hZBKiIzIv4m7ZBjn2ZC";
+
+function sendText(sender, text) {
+    let messageData = {text: text};
+    if (text.includes('options')) {
+        return getMessageTemplate(sender, text);
+    }
+    request({
+        url: "https://graph.facebook.com/v4.0/me/messages",
+        qs: {access_token : token},
+        method: "POST",
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log("error");
+        } else if (response.body.error) {
+            console.log("response body error");
+        }
+    });
+}
+
 app.listen(app.get('port'), function() {
     console.log('running port')
 })
